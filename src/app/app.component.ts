70787e786b487c447ab474368598878815a6907d
Guest User <Guest@Epicodus-3E.local>
2018-03-26 15:22:18 -0700

    basic angular setup for hard coded recipe box.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/task.model'
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-crazzzzzy',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  inputTask1: string =prompt('enter recipe name');
+  inputTask2: string =prompt('enter recipe ingredients');
+  inputTask3: string =prompt('enter recipe directions');
+  recipes: Recipe[] = [
+    new Recipe('Cheesey Bread','cheese and dough', 'mix cheese and dough tada'),
+    new Recipe('Boiled Fish', 'worm on hook plus fishing pole plus 3 hours', 'catch a fish, boil'),
+    new Recipe(this.inputTask1, this.inputTask2, this.inputTask3)
+  ];
+  currentTime = new Date();
+  day: number = this.currentTime.getDate();
 }
