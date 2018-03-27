8def5decc3f0cc58abb4339186503bc6cb0ffd0a
dan kiss and roger yee <roger@>
2018-03-27 11:44:48 -0700

    add way to select recipe and change its rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,14 @@ export class AppComponent {
     new Recipe('Cheesey Bread', 'cheese and dough', 'mix cheese and dough tada', 9),
     new Recipe('Boiled Fish', 'worm on hook plus fishing pole plus 3 hours', 'catch a fish, boil', 1),
   ];
-  selectedRecipe: Recipe= this.recipes[0];
   currentTime = new Date();
   day: number = this.currentTime.getDate();
-
-  addRecipe() {
-    alert('add the recipe');
+  month: number = this.currentTime.getMonth();
+  year: number = this.currentTime.getFullYear();
+  selectedRecipe: Recipe= this.recipes[0];
+  addRecipe(single) {
+    // alert('add the recipe');
+    this.selectedRecipe = single;
   }
   ratingColor(single) {
     if (single.rating <= 3) {
